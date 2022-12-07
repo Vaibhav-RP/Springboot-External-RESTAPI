2be5b929f6f2340d748b4efb800ca6877cd81e24
Vaibhav-RP <vrp8698231262@gmail.com>
2022-12-07 10:49:10 +0530

    COMMIT_CHANGES

--- src/main/java/com/example/demo/controller/GenderDataController.java
+++ src/main/java/com/example/demo/controller/GenderDataController.java
@@ -4,6 +4,8 @@ package com.example.demo.controller;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,24 +22,38 @@ public class GenderDataController {
 
     // when we hit  http://localhost:8080/api  data will be added to the database.
     @GetMapping("/api")                    
-    public DataEntity saveGenderData(){
-         return genderDataService.getGenderData();
+    public ResponseEntity<DataEntity> saveGenderData(){
+        DataEntity data =  genderDataService.getGenderData();
+        return new ResponseEntity<>(data, HttpStatus.CREATED);
     }
 
 
     // when we hit  http://localhost:8080/get-all-data  all data present in the database will be displayed.
     @GetMapping("/get-all-data")           
-    public List<DataEntity> getAll(){
-        return genderDataService.getAllData();
+    public ResponseEntity<List<DataEntity>> getAll(){
+        
+        List<DataEntity> data = genderDataService.getAllData();
+
+        if (data.isEmpty()) {
+          return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } else
+          return new ResponseEntity<>(data, HttpStatus.CREATED);
     }
+    
 
 
     // when we hit  http://localhost:8080/get-data/id  data of that id will be displayed.
     @GetMapping("/get-data/{id}")           
-    public Optional<DataEntity> getById(@PathVariable("id") int id){
-        return genderDataService.getDataById(id);
+    public ResponseEntity<DataEntity> getById(@PathVariable("id") int id){
+        Optional<DataEntity> data = genderDataService.getDataById(id);
+
+        if (data.isEmpty()) {
+          return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } else
+          return new ResponseEntity<>(data.get(), HttpStatus.CREATED);
+      }
     }
 
   
     
-}
+
